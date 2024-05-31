7732231505e8990c8c7359cf14f54bb6ba459169
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-05-31 15:06:09 -0300

    moving the parse of short/extended fields from parse method name to separated methods

--- equipa_viewer.py
+++ equipa_viewer.py
@@ -25,22 +25,20 @@ class Equipa:
 
     ext_name: str
     short_name: str
-    #country: str = ''
+    country: str = ''
     #colours: list = list()
-    #coach: str = ''
-    #level: int = 0
+    level: int = 0
+    coach: str = ''
     #players: list = list()
 
     def __str__(self):
-        #return f'''
-        #    name: {self.full_name}
-        #    abrev. name: {self.short_name}
-        #    level: {self.level}
-        #    country: {self.country}
-        #    coach: {self.coach}
-        #    players: {self.players}
-        #    '''
-        return f'''extended name: {self.ext_name}, short name: {self.short_name}'''
+        return (
+            f'extended name:\t{self.ext_name}\n'
+            f'short name:\t{self.short_name}\n'
+            f'country:\t{self.country}\n'
+            f'level:\t\t{self.level}\n'
+            f'coach:\t{self.coach}'
+        )
 
 
 class EquipaParser:
@@ -58,28 +56,35 @@ class EquipaParser:
             if not self.has_equipa_header(data):
                 raise EquipaHeaderNotFound
 
-            size_ext_name = data[50]
-            ext_name = self.parse_field(data, 51, size_ext_name)
-
-            size_short_name = data[51 + size_ext_name]
-            short_name = self.parse_field(data, 52 + size_ext_name,
-                                          size_short_name)
+            ext_name = self.parse_ext_name(data)
+            short_name = self.parse_short_name(data, len(ext_name))
 
             return Equipa(ext_name=ext_name, short_name=short_name)
+
+    def parse_ext_name(self, data):
+        size = data[50]
+
+        return self.parse_field(data, 51, size)
+
+    def parse_short_name(self, data, size_previous):
+        size = data[51 + size_previous]
+
+        return self.parse_field(data, 52 + size_previous, size)
 
     def parse_field(self, data, offset, size):
         ret = ''
 
         for i in range(offset, offset + size):
-            char = (data[i] - data[i-1]) & 0xff
+            char = (data[i] - data[i - 1]) & 0xff
             ret += chr(char)
 
         return ret
 
-def main(equipa_file):
-    equipa = EquipaParser(equipa_file).parse()
 
-    print(equipa)
+def main(equipa_file):
+    ep = EquipaParser(equipa_file)
+
+    print(ep.parse())
 
 
 if __name__ == "__main__":
