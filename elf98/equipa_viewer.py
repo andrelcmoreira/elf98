2c7cc2b1c2bd8ee6ec79e5796561cf7365e82bc9
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-07-05 00:49:52 -0300

    equipa_viewer: fixing __str__ method of Equipa class

--- elf98/equipa_viewer.py
+++ elf98/equipa_viewer.py
@@ -49,7 +49,7 @@ class Equipa:
             f'extended name:\t{self.ext_name}\n'
             f'short name:\t{self.short_name}\n'
             f'country:\t{self.country}\n'
-            f'colors:\t{self.colors} (text, background)\n'
+            f'colors:\t\t{self.colors} (text, background)\n'
             f'level:\t\t{self.level}\n'
             f'coach:\t\t{self.coach}\n'
             f'players:\t{self.players}'
