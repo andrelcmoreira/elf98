9ebd8884b7379d24c3381908cfe6e332ccb2eb69
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-07-30 22:33:02 -0300

    providers/espn: adding equipas starting with 'o', 'p', 'q' and 'r'

--- elf98/elf98.py
+++ elf98/elf98.py
@@ -9,18 +9,18 @@ from error.not_found import EquipaNotFound
 def parse_args():
     parser = ArgumentParser(prog=argv[0])
 
-    parser.add_argument('-v', '--view-equipa', action='store_true',
-                        help="Print the equipa's data")
-    parser.add_argument('-u', '--update-equipa', action='store_true',
-                        help="Update an equipa specified by '-f' option")
     parser.add_argument('-b', '--bulk-update', action='store_true',
                         help='Update all the game equipas')
-    parser.add_argument('-f', '--equipa-file', metavar='file',
-                        help='Elifoot equipa file name')
     parser.add_argument('-d', '--equipas-dir', metavar='directory',
                         help="Elifoot equipa's directory")
+    parser.add_argument('-f', '--equipa-file', metavar='file',
+                        help='Elifoot equipa file name')
     parser.add_argument('-p', '--provider', metavar='provider',
                         help='Team data provider')
+    parser.add_argument('-u', '--update-equipa', action='store_true',
+                        help="Update an equipa specified by '-f' option")
+    parser.add_argument('-v', '--view-equipa', action='store_true',
+                        help="Print the equipa's data")
 
     # no arguments provided
     if len(argv) == 1:
