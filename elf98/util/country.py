ea79065bfaf84cd84d7ae9ce005456955f9d362a
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-08-22 00:04:56 -0300

    elf98: adding more countries to get_country method

--- elf98/util/country.py
+++ elf98/util/country.py
@@ -5,6 +5,8 @@ def get_country(country):
     cnt = country[0:3].upper()
 
     match cnt:
+        case 'EGI': return 'EGT'
+        case 'GAN': return 'GNA'
         case 'VEN': return 'VNZ'
         case 'REP': return 'RCH'
 
