dabff827cbe8cbda9ae63cbe351570234e11eeb5
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-08-22 22:36:02 -0300

    elf98: adding more countries on get_country method

--- elf98/util/country.py
+++ elf98/util/country.py
@@ -5,6 +5,12 @@ def get_country(country):
     cnt = country[0:3].upper()
 
     match cnt:
+        # TODO: missing eslovenia flag on 'FLAGS' dir
+        # TODO: missing iraq flag on 'FLAGS' dir
+        case 'AFR': return 'AFS'
+        case 'CAP': return 'CAV'
+        case 'CHA': return 'CHD'
+        case 'CHI': return 'CHL'
         case 'EGI': return 'EGT'
         case 'GAN': return 'GNA'
         case 'VEN': return 'VNZ'
