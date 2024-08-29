724e72ec91e424a26591f8dbddb3cdb7ad0b68bb
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-08-28 22:17:00 -0300

    elf98: using the full country's name as key to _COUNTRIES dict

--- elf98/util/country.py
+++ elf98/util/country.py
@@ -1,28 +1,27 @@
 from unidecode import unidecode
 
 
-# TODO: use full country name as key
 _COUNTRIES = {
-    'AFR': 'AFS',
-    'CAP': 'CAV',
-    'CHA': 'CHD',
-    'CON': 'CNG',
-    'COS': 'CMF',
-    'CHI': 'CHL',
-    'CZE': 'RCH',
-    'COR': 'CRS',
+    'África do Sul': 'AFS',
+    'Cape Verde Islands': 'CAV',
+    'Chade': 'CHD',
+    'Congo (Brazavile)': 'CNG',
+    'Costa do Marfim': 'CMF',
+    'Chile': 'CHL',
+    'Czechia': 'RCH',
+    'Coreia do Sul': 'CRS',
     'IRA': '', # ira
-    'EGI': 'EGT',
-    'ESL': 'EVN',
-    'GAN': 'GNA',
+    'Egito': 'EGT',
+    'Eslovênia': 'EVN',
+    'Gana': 'GNA',
     'PAI': '', # walles
     'USA': 'EUA',
-    'VEN': 'VNZ',
-    'REP': 'RCH'
+    'Venezuela': 'VNZ',
+    'República da Sérvia': 'SER'
 }
 
 
 def get_country(country):
-    cnt = unidecode(country[0:3]).upper()
-
-    return _COUNTRIES[cnt] if cnt in _COUNTRIES else cnt
+    return _COUNTRIES[country] \
+        if country in _COUNTRIES \
+        else unidecode(country[0:3]).upper()
