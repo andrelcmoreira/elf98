72b894afaedf1934c42d726435f5cdcf46eb6656
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-02-11 21:50:07 -0300

    elf98: adding more countries to the mapping

--- elf98/util/country.py
+++ elf98/util/country.py
@@ -1,40 +1,44 @@
 from unidecode import unidecode
 
 
-# the countries below are not mapped by the game:
-# - Curaçao
-# - Eritreia
-# - Irã
-# - Kosovo
 # FIXME: the name of the countries are provider specific
 _COUNTRIES = {
     'África do Sul': 'AFS',
     'Cape Verde Islands': 'CAV',
+    'Cazaquistão': '',
     'Chade': 'CHD',
     'Congo (Brazavile)': 'CNG',
     'Costa do Marfim': 'CMF',
     'Costa Rica': 'CRC',
     'Chile': 'CHL',
     'China': 'CHN',
+    'China PR': 'CHN',
     'Chipre': 'CHP',
+    'Curaçao': '',
     'Czechia': 'RCH',
     'Coreia do Sul': 'CRS',
     'Egito': 'EGT',
+    'Eritreia': '',
     'Eslováquia': 'EVQ',
     'Eslovênia': 'EVN',
     'Gana': 'GNA',
     'Gâmbia': 'GMB',
     'Granada': 'GRN',
+    'Irã': '',
+    'Kosovo': '',
+    'Liechtenstein': '',
     'Namíbia': 'NMI',
     'Nova Zelândia': 'NZE',
     'País de Gales': 'WAL',
+    'Trinidad e Tobago': 'TND',
     'USA': 'EUA',
     'Venezuela': 'VNZ',
     'Republic of Ireland': 'IRL',
     'República da Sérvia': 'SER',
     'República Democrática do Congo': 'CNG',
     'República Centro-Africana': 'RCA',
-    'República Dominicana': 'RDO'
+    'República Dominicana': 'RDO',
+    'Zimbábue': 'ZBW'
 }
 
 
